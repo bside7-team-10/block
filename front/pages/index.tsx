fd9018e2da9ebe9beebe6d22599c33e70ce39900
parksil0 <parksil00@naver.com>
2021-09-20 10:36:19 +0900

    feat : import kakao map api script tag

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -1,8 +1,17 @@
 import type { NextPage } from "next";
+import Head from "next/head";
+
 import Home from "../components/Home";
 
 const HomePage: NextPage = () => {
-  return <Home />;
+  return (
+    <>
+      <Head>
+        <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey="></script>
+      </Head>
+      <Home />
+    </>
+  );
 };
 
 export default HomePage;
