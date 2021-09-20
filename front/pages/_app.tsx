0e5381121ea3bad16baeac3e925c6f5369485068
saegeullee <lemix777@naver.com>
2021-09-20 22:19:09 +0900

    remove material UI sync and applied antd design css

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -1,17 +1,14 @@
 import "../styles/globals.css";
+import "antd/dist/antd.css";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import withRedux from "next-redux-wrapper";
-import { StylesProvider } from "@material-ui/core/styles";
-
 import { store } from "../state/store";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <StylesProvider injectFirst>
-        <Component {...pageProps} />
-      </StylesProvider>
+      <Component {...pageProps} />
     </Provider>
   );
 }
