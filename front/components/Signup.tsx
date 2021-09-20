ff13bc33031f7843d02a21658d0eba8bfce13be5
saegeullee <lemix777@naver.com>
2021-09-20 22:21:41 +0900

    edit signup UI with updated common fields

--- front/components/Signup.tsx
+++ front/components/Signup.tsx
@@ -1,49 +1,105 @@
 import { useForm } from "react-hook-form";
 import styled from "styled-components";
 import { Button } from "@material-ui/core";
+import router from "next/router";
 
 import { User } from "../state/user";
 import { useActions } from "../hooks/use-actions";
-import InputField from "../components/common/fields/InputField";
-import router from "next/router";
+import Fields from "../components/common/fields/Fields";
 
 const defaultValues = {
   email: "",
   password: "",
   confirmPassword: "",
   nickName: "",
+  birthday: "",
+  gender: "",
 };
 
 const Signup = () => {
-  const { control, handleSubmit, setError, getValues, clearErrors } = useForm({ defaultValues });
+  const {
+    control,
+    handleSubmit,
+    getValues,
+    register,
+    formState: { errors },
+  } = useForm({ defaultValues });
   const { userSignup } = useActions();
 
   const onSubmit = (data: User) => {
     userSignup(data, onSigupSuccessCallback);
   };
 
-  const onConfirmPasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const confirmPassword = event.target?.value;
-    if (getValues("password") !== confirmPassword) {
-      setError("confirmPassword", { message: "비밀번호가 틀립니다." });
-      return;
-    }
-    clearErrors("confirmPassword");
-  };
-
   const onSigupSuccessCallback = () => {
     router.push("/");
   };
+
+  const checkConfirmPassword = (confirmPassword: string) => {
+    return confirmPassword === getValues("password");
+  };
+
+  register("confirmPassword", { validate: checkConfirmPassword });
 
   return (
     <div>
       <Wrapper>
         <Title>회원가입</Title>
         <form>
-          <InputField name="email" label="Enter email" control={control} required={true} variant="filled" />
-          <InputField name="password" label="Enter password" control={control} required={true} variant="filled" />
-          <InputField name="confirmPassword" label="Confirm password" control={control} required={true} variant="filled" onChange={onConfirmPasswordChange} />
-          <InputField name="nickName" label="Enter Nickname" control={control} required={true} variant="filled" />
+          <Fields
+            name="email"
+            size="large"
+            placeholder="사용할 이메일 주소를 입력하세요"
+            prefix="MAIL"
+            control={control}
+            error={errors.email}
+            required
+          />
+          <Fields
+            name="password"
+            size="large"
+            placeholder="비밀번호"
+            prefix="PW"
+            control={control}
+            error={errors.password}
+            required
+          />
+          <Fields
+            name="confirmPassword"
+            size="large"
+            placeholder="비밀번호 확인"
+            prefix="PW"
+            control={control}
+            error={errors.confirmPassword}
+          />
+          <Fields
+            name="nickName"
+            size="large"
+            placeholder="닉네임을 입력하세요"
+            prefix="ID"
+            control={control}
+            error={errors.nickName}
+            required
+          />
+          <Fields
+            name="birthday"
+            type="date"
+            size="large"
+            placeholder="출생년도를 입력하세요"
+            prefix="YY"
+            control={control}
+            error={errors.birthday}
+            required
+          />
+          <Fields
+            name="gender"
+            type="radio"
+            size="large"
+            prefix="XY"
+            control={control}
+            error={errors.gender}
+            options={genderOptions}
+            required
+          />
         </form>
         <Button size="large" onClick={handleSubmit(onSubmit)} variant="contained" color="primary">
           회원가입
@@ -55,11 +111,20 @@ const Signup = () => {
 
 export default Signup;
 
+interface FormSelectOptions {
+  [index: number]: { label: string; value: string };
+}
+
+const genderOptions: FormSelectOptions = [
+  { label: "남성", value: "man" },
+  { label: "여성", value: "woman" },
+];
+
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  width: 300px;
+  width: 350px;
   margin-left: auto;
   margin-right: auto;
   margin-top: 100px;
