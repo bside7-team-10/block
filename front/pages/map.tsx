aa8be487af8365a885a4675f878b0eef6c25f5c0
parksil0 <parksil00@naver.com>
2021-09-20 10:35:57 +0900

    feat : 단순 화면 설정

---
+++ front/pages/map.tsx
@@ -0,0 +1,14 @@
+import { NextPage } from "next";
+
+import Layout from "../components/common/Layout";
+import Map from "../components/Map";
+
+const MapPage: NextPage = () => {
+  return (
+    <Layout>
+      <Map />
+    </Layout>
+  );
+};
+
+export default MapPage;
