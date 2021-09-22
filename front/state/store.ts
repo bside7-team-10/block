f9cc64fc327c4559615fbad11e30881760d47353
saegeullee <lemix777@naver.com>
2021-09-22 22:37:10 +0900

    applied using composeWithDevTools

--- front/state/store.ts
+++ front/state/store.ts
@@ -1,7 +1,6 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
-import reducers from "./reducers";
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import reducers from './reducers';
 
-const composeEnhancers = (process.env.NODE_ENV === "development" && typeof window !== "undefined" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
-
-export const store = createStore(reducers, {}, composeEnhancers(applyMiddleware(thunk)));
+export const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)));
