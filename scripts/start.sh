9f21f9144714ba7e3a7a7b4ae1de9bcd8643110d
jjlee <cooco119@gmail.com>
2021-10-02 22:44:36 +0900

    Fix start.sh stop.sh scripts

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,34 +3,34 @@
 INFRA="dev"
 
 # server
-if [ "$DEPLOYMENT_GROUP_NAME" == "block-server-deploy-group-dev"]
+if [ "$DEPLOYMENT_GROUP_NAME" == "block-server-deploy-group-dev" ]
 then
     INFRA="dev"
     docker pull public.ecr.aws/b1b4k6o9/block-server:$INFRA-latest
-    docker-compose --env-file "scripts/.env.$INFRA" -f "scripts/docker-compose.server.yaml" --project-directory $(pwd) up -d 
+    docker-compose --env-file "scripts/.env.$INFRA" -f "scripts/docker-compose.server.yaml" --project-directory "$(pwd)" up -d 
 
-elif [ "$DEPLOYMENT_GROUP_NAME" == "block-server-deploy-group-staging"]
+elif [ "$DEPLOYMENT_GROUP_NAME" == "block-server-deploy-group-staging" ]
 then
     INFRA="staging"
     docker pull public.ecr.aws/b1b4k6o9/block-server:$INFRA-latest
-    docker-compose --env-file "scripts/.env.$INFRA" -f "scripts/docker-compose.server.yaml" --project-directory $(pwd) up -d 
+    docker-compose --env-file "scripts/.env.$INFRA" -f "scripts/docker-compose.server.yaml" --project-directory "$(pwd)" up -d 
 
-elif [ "$DEPLOYMENT_GROUP_NAME" == "block-server-deploy-group-prod"]
+elif [ "$DEPLOYMENT_GROUP_NAME" == "block-server-deploy-group-prod" ]
 then
     INFRA="prod"
     docker pull public.ecr.aws/b1b4k6o9/block-server:$INFRA-latest
-    docker-compose --env-file "scripts/.env.$INFRA" -f "scripts/docker-compose.server.yaml" --project-directory $(pwd) up -d 
+    docker-compose --env-file "scripts/.env.$INFRA" -f "scripts/docker-compose.server.yaml" --project-directory "$(pwd)" up -d 
 
 # app - TODO
-elif [ "$DEPLOYMENT_GROUP_NAME" == "block-app-deploy-group-dev"]
+elif [ "$DEPLOYMENT_GROUP_NAME" == "block-app-deploy-group-dev" ]
 then
     INFRA="dev"
     docker pull public.ecr.aws/b1b4k6o9/block-app:$INFRA-latest
-elif [ "$DEPLOYMENT_GROUP_NAME" == "block-app-deploy-group-staging"]
+elif [ "$DEPLOYMENT_GROUP_NAME" == "block-app-deploy-group-staging" ]
 then
     INFRA="staging"
     docker pull public.ecr.aws/b1b4k6o9/block-app:$INFRA-latest
-elif [ "$DEPLOYMENT_GROUP_NAME" == "block-app-deploy-group-prod"]
+elif [ "$DEPLOYMENT_GROUP_NAME" == "block-app-deploy-group-prod" ]
 then
     INFRA="prod"
     docker pull public.ecr.aws/b1b4k6o9/block-app:$INFRA-latest
