18ee4c9d47311bdefbbd15419ae5935686f414c6
parksil0 <parksil00@naver.com>
2021-10-11 18:55:06 +0900

    chore: update small changes
    - package-lock.json 제거
    - tsconfig.json에 불필요한 설정 제거
    - 사용하지 않는 코드 제거

--- front/state/action-creators/loginAction.test.ts
+++ front/state/action-creators/loginAction.test.ts
@@ -5,8 +5,6 @@ import thunk from 'redux-thunk';
 import { userLogin } from './index';
 import { User } from '../user';
 
-jest.mock('../service/index');
-
 const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
 
@@ -24,9 +22,7 @@ const testCallback = () => {
 
 test('userLogin action creator test ', async () => {
   const store = mockStore({});
-
   store.dispatch(userLogin(user, testCallback) as any);
   const actions = store.getActions();
-
   expect(actions[0]).toEqual({ type: ActionType.USER_LOGIN_REQUEST });
 });
