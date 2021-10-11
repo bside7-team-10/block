d679a281f882702d33f47b301787e40621966164
saegeullee <lemix777@naver.com>
2021-10-12 01:08:09 +0900

    [FE] chore: 모바일 웹뷰에서 input창 클릭시 화면 확대되지 않도록

--- front/components/common/Layout.tsx
+++ front/components/common/Layout.tsx
@@ -12,6 +12,10 @@ const Layout = ({ children }: LayoutProps) => {
     <>
       <Head>
         <KakaoScript />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+        />
       </Head>
       <Wrapper>{children}</Wrapper>
     </>
