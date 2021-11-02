58414adca1704a6a5ebbf5012ad171ffc6d7e6b0
parksil0 <parksil00@naver.com>
2021-11-03 00:35:00 +0900

    chore : SelectButtonField 재설정
    - 재사용성에 용이하도록 로직 변경

--- front/components/common/fields/SelectButtonField.tsx
+++ front/components/common/fields/SelectButtonField.tsx
@@ -1,18 +1,21 @@
 import React, { useState } from 'react';
 import { Controller } from 'react-hook-form';
 import styled from 'styled-components';
-import { PRIMARY_COLOR2, WHITE_COLOR, BLACK_COLOR } from '../../../utils/theme/theme';
+import { THEME_COLOR1, WHITE_COLOR, BLACK_COLOR, GREY_3 } from '../../../utils/theme/theme';
 
 export interface SelectButtonFieldProps {
   name: string;
   control: any;
   required: boolean;
   options: any;
+  page: string; // 사용하는 페이지 e.g.. signup, write
 }
 
 const SelectButtonField = (props: SelectButtonFieldProps) => {
-  const { name, control, options = null } = props;
+  const { name, control, options = null, page } = props;
   const [localOptions, setLocalOptions] = useState(options);
+
+  const StyledButton = whichPage(page);
 
   const onOptionClicked = (value: string, onChange: any) => {
     const newOptions = localOptions.map((option: any) => {
@@ -50,11 +53,22 @@ const SelectButtonField = (props: SelectButtonFieldProps) => {
 
 export default SelectButtonField;
 
+function whichPage(page: string) {
+  switch (page) {
+    case 'signup':
+      return StyledSignupButton;
+    case 'write':
+      return StyledWriteButton;
+    default:
+      return StyledSignupButton;
+  }
+}
+
 interface ButtonProps {
   checked: boolean;
 }
 
-const StyledButton = styled.button`
+const StyledSignupButton = styled.button`
   padding: 6px 12px;
   height: 33px;
   box-sizing: border-box;
@@ -64,7 +78,21 @@ const StyledButton = styled.button`
   margin-right: 5px;
   margin-bottom: 5px;
   border: ${({ checked }: ButtonProps) =>
-    checked ? '1px solid transparent' : `1px solid ${PRIMARY_COLOR2}`};
-  color: ${({ checked }: ButtonProps) => (checked ? `${BLACK_COLOR}` : `${PRIMARY_COLOR2}`)};
+    checked ? '1px solid transparent' : `1px solid ${THEME_COLOR1}`};
+  color: ${({ checked }: ButtonProps) => (checked ? `${BLACK_COLOR}` : `${THEME_COLOR1}`)};
   background-color: ${({ checked }: ButtonProps) => (checked ? `${WHITE_COLOR}` : 'transparent')};
+`;
+
+const StyledWriteButton = styled.button`
+  padding: 3px 6px;
+  height: 21px;
+  box-sizing: border-box;
+  border-radius: 37px;
+  font-weight: 500;
+  font-size: 10px;
+  margin-right: 3px;
+  margin-bottom: 6px;
+  border: 1px solid ${GREY_3};
+  color: ${({ checked }: ButtonProps) => (checked ? `${WHITE_COLOR}` : `${GREY_3}`)};
+  background-color: ${({ checked }: ButtonProps) => (checked ? `${GREY_3}` : 'transparent')};
 `;
