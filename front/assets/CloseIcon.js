c03cb5c06348d24c6caa19dab22b823c9560770a
parksil0 <parksil00@naver.com>
2021-11-03 00:05:52 +0900

    chore : ui 추가 수정

--- front/assets/CloseIcon.js
+++ front/assets/CloseIcon.js
@@ -2,18 +2,18 @@ import React from 'react';
 
 const CloseIcon = () => {
   return (
-    <svg width="12" height="12" viewBox="0 3 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg width="14" height="14" viewBox="0 3 24 24" fill="#a5a5a5" xmlns="http://www.w3.org/2000/svg">
       <path
         d="M18 6L6 18"
-        stroke="#0C0C33"
-        strokeWidth="2"
+        stroke="#a5a5a5"
+        strokeWidth="3"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
         d="M6 6L18 18"
-        stroke="#0C0C33"
-        strokeWidth="2"
+        stroke="#a5a5a5"
+        strokeWidth="3"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
