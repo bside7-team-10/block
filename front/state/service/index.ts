95d605d75e1d5a5a76e79bf37aeae78274b35229
jjlee <cooco119@gmail.com>
2021-11-14 17:30:11 +0900

    [FE] feat: Implment sample create post api

--- front/state/service/index.ts
+++ front/state/service/index.ts
@@ -1,11 +1,15 @@
 import { Cookies } from 'react-cookie';
 
 import { SignInRequest, SignUpRequest } from '../../_generated/UserProtocol_pb';
+import { CreatePostRequest, LocationDto, UploadImageResultRequest } from '../../_generated/PostProtocol_pb';
 import { LoginUser } from '../loginUser';
 import { UserProtocolClient } from '../../_generated/UserProtocol_pb_service';
+import { PostProtocolClient } from '../../_generated/PostProtocol_pb_service';
 import { User } from '../user';
 import { Post } from '../post';
+import { grpc } from '@improbable-eng/grpc-web';
 
+const serverUrl = "http://localhost:8081";
 export interface ServiceInterface {
   signup: (user: User) => Promise<any>;
   login: (user: User) => Promise<any>;
@@ -25,7 +29,7 @@ const Service = () => {
       req.setBirthday(birthday);
       req.setAvatar('1.png');
       req.setGender(gender);
-      const userClient = new UserProtocolClient('http://52.78.170.114:8081');
+      const userClient = new UserProtocolClient(serverUrl);
       userClient.signUp(req, (err, res) => {
         if (err !== null) {
           console.error(err);
@@ -43,7 +47,7 @@ const Service = () => {
       const req = new SignInRequest();
       req.setEmail(email);
       req.setPassword(password);
-      const userClient = new UserProtocolClient('http://52.78.170.114:8081');
+      const userClient = new UserProtocolClient(serverUrl);
       userClient.signIn(req, (err, res) => {
         if (err !== null) {
           reject('아이디 또는 비밀번호가 다릅니다.');
@@ -73,16 +77,33 @@ const Service = () => {
     });
   };
 
-  self.addPost = (data: Post) => {
+  self.addPost = (post: Post) => {
     return new Promise((resolve, reject) => {
-      const { content } = data;
-      if (content) {
-        resolve('게시글이 등록되었습니다.');
-      } else {
-        reject('오류가 발생했습니다.');
-      }
+      const req = new CreatePostRequest();
+      req.setAuthor("jjlee");
+      req.setContent(post.content);
+      const loc = new LocationDto();
+      loc.setLat(127);
+      loc.setLong(38);
+      req.setLocation(loc);
+      const postClient = new PostProtocolClient(serverUrl);
+      const headers = new grpc.Metadata();
+      const cookies = new Cookies();
+      headers.append("Authorization", "Bearer " + cookies.get("accessToken"));
+      postClient.createPost(req, headers, async (err, res) => {
+        if (err !== null) {
+          console.error(err);
+          reject(err);
+        } else if (res !== null) {
+          console.log(res);
+          // const imageUrl = res.getUploadimageurl();
+          // const result = await axios.put(imageUrl, post.imageSource);
+          // console.log("upload image: ", result);
+          resolve(res);
+        }
+      });
     });
-  };
+  }
 
   return self;
 };
