36e3713ce393fa491daa2a03278da1238a80e511
JaeJun.Lee <34636736+cooco119@users.noreply.github.com>
2021-11-20 03:02:23 +0900

    Update test-config.ts

--- front/config/test-config.ts
+++ front/config/test-config.ts
@@ -1,7 +1,7 @@
 const config = {
-  NEXT_KAKAO_MAP_URL: process.env.NEXT_KAKAO_MAP_URL || 'https://dapi.kakao.com/v2/maps/sdk.js?appkey=9c85f7ab84cf89ee918effa7025dac19',
-  NEXT_GOOGLE_MAP_URL: process.env.NEXT_GOOGLE_MAP_URL || 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBTUuPbmwzcJfplBlVDZOkQw8KMDcI7dmE ',
-  GOOGLE_MAP_KEY: process.env.GOOGLE_MAP_KEY || 'AIzaSyBTUuPbmwzcJfplBlVDZOkQw8KMDcI7dmE',
+  NEXT_KAKAO_MAP_URL: process.env.NEXT_KAKAO_MAP_URL || '',
+  NEXT_GOOGLE_MAP_URL: process.env.NEXT_GOOGLE_MAP_URL || '',
+  GOOGLE_MAP_KEY: process.env.GOOGLE_MAP_KEY || '',
 };
 
 export default config;
